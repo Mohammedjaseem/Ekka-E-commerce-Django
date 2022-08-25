bd305d35df30f8c048488c1347eadbb175a08d71
Mohammed Jaseem Tp <56159302+Mohammedjaseem@users.noreply.github.com>
2022-08-25 10:32:03 +0530

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -11,3 +11,16 @@ class Payment(models.Model):
 
     def __str__(self):
         return self.payment_id
+
+class Order(models.Model):
+    STATUS = (
+        ('new', 'New'),
+        ('Accepted', 'Accepted'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    )
+
+    user = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
+    Payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, null=True, blank=True)
+    order_number = models.CharField(max_length=20)
+    first_name = 

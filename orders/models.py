8509fad639a22d50863012caddc1bcffa3ab79cb
Mohammed Jaseem Tp <56159302+Mohammedjaseem@users.noreply.github.com>
2022-08-25 10:15:36 +0530

    requests

---
+++ orders/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Payment(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    payment_id = models.CharField(max_length=100)
+    payment_method = models.CharField(max_length=100)
+    amount_paid = models.FloatField() #total ampount paid
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.payment_id

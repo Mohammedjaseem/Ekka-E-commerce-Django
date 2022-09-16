78a5383e81afbc97dd1c1c7fab37aae510557482
Mohammed Jaseem Tp <56159302+Mohammedjaseem@users.noreply.github.com>
2022-09-16 11:26:22 +0530

    AWS S3

--- Ekka_Project/settings.py
+++ Ekka_Project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'Ekka_Admin_App',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -132,10 +133,32 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static/')
+# ]
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static/')
+    'static/',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# DEFAULT_FILE_STORAGE = 'clickkart.media_storages.MediaStorage'
+
+
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
